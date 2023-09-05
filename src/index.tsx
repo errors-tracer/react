ce5f5e5b8f46f7a8f51e18cef37811e738f67bd6
Elsiddig Ahmed <elsiddig190@gmail.com>
2023-09-04 21:22:23 -0500

    delete example folder

--- src/index.tsx
+++ src/index.tsx
@@ -5,74 +5,69 @@ import {
   isChrome,
   isEdge,
   isSafari,
-  isFirefox,
-} from "react-device-detect";
-
+  isFirefox
+} from 'react-device-detect'
 
 interface Props {
-  fallback: React.ReactNode;
-  children: React.ReactNode;
+  fallback: React.ReactNode
+  children: React.ReactNode
   configs: {
-    appName: string;
-    appSecret: string;
+    appName: string
+    appSecret: string
   }
 }
 
 interface State {
-  hasError: boolean;
-  error: any;
+  hasError: boolean
+  error: any
   errorInfo: any
 }
 
 export class ErrorBoundary extends React.Component<Props, State> {
   constructor(props: any) {
-    super(props);
-    this.state = { hasError: false, error: {}, errorInfo: {} };
+    super(props)
+    this.state = { hasError: false, error: {}, errorInfo: {} }
   }
 
-  static getDerivedStateFromError(error: any) {
+  static getDerivedStateFromError() {
     // Update state so the next render will show the fallback UI.
 
-    return { hasError: true };
+    return { hasError: true }
   }
 
   async componentDidCatch(error: any, errorInfo: any) {
- 
-    let agent = "Unknown";
+    let agent = 'Unknown'
 
-    if (isChrome) agent = "Chrome";
-    if (isIE) agent = "IE";
-    if (isEdge) agent = "Edge";
-    if (isSafari) agent = "Safari";
-    if (isFirefox) agent = "Firefox";
+    if (isChrome) agent = 'Chrome'
+    if (isIE) agent = 'IE'
+    if (isEdge) agent = 'Edge'
+    if (isSafari) agent = 'Safari'
+    if (isFirefox) agent = 'Firefox'
 
     await fetch(`http://api.errorstracer.com/v0.1/trace/react`, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
-        
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         credentials: this.props.configs,
         data: {
           error: error.message,
-        stack: error.stack,
-        platform: "Unknown",
-        agent,
+          stack: error.stack,
+          platform: 'Unknown',
+          agent
         }
-      }),
-    });
+      })
+    })
 
-    this.setState({ error, errorInfo });
+    this.setState({ error, errorInfo })
   }
 
   render() {
-    if (this.state?.hasError) { 
-      return this.props.fallback;
+    if (this.state?.hasError) {
+      return this.props.fallback
     }
 
-    return this?.props?.children;
+    return this?.props?.children
   }
 }
-
- 
